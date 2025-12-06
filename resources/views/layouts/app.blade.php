7387e9d9a8eae5e70683a82cc5e2e21bc5458132
Tung Pham <tung.42@gmail.com>
2025-12-07 05:26:12 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,6 +89,14 @@
       crossorigin="anonymous"
     ></script>
 
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "t9evh6woav");
+    </script>
+
     <!-- Custom CSS -->
     <style>
         :root {
